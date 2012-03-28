d812169cd67a4e1e0ebf4ec271477b08c1cfeead
Aaron Evans <fijiaaron@fast-mail.org>
2012-03-28 12:42:57 -0500

    added PEI field to ScipioDialog

--- floreantpos/src/com/floreantpos/ui/dialog/ScipioDialog.java
+++ floreantpos/src/com/floreantpos/ui/dialog/ScipioDialog.java
@@ -22,21 +22,25 @@ public class ScipioDialog extends POSDialog {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.floreantpos.swing.PosButton btnCancel;
     private com.floreantpos.swing.PosButton btnOk;
+    private javax.swing.JLabel lblPEI;
     private javax.swing.JLabel lblPIN;
     private javax.swing.JLabel lblTEI;
     private com.floreantpos.ui.TitlePanel titlePanel1;
+    private javax.swing.JFormattedTextField txtPEI;
     private javax.swing.JFormattedTextField txtPIN;
     private javax.swing.JFormattedTextField txtTEI;
     // End of variables declaration//GEN-END:variables
 	
     MaskFormatter teiFormatter = null;
 	MaskFormatter pinFormatter = null;
+	MaskFormatter peiFormatter = null;
 	
 	/**
 	 * Creates new form ScipioDialog
 	 */
 	public ScipioDialog() {
 		super(Application.getPosWindow(), true, false);
+		
 		System.out.println("in ScipioDialog constructor");
 		
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -44,6 +48,8 @@ public class ScipioDialog extends POSDialog {
         try {
 			teiFormatter = new MaskFormatter("#####");
 			pinFormatter = new MaskFormatter("####");
+			peiFormatter = new MaskFormatter("######");
+			
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
@@ -60,14 +66,16 @@ public class ScipioDialog extends POSDialog {
 	 */
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-    	
+
         titlePanel1 = new com.floreantpos.ui.TitlePanel();
         lblTEI = new javax.swing.JLabel();
         btnOk = new com.floreantpos.swing.PosButton();
         btnCancel = new com.floreantpos.swing.PosButton();
-        txtTEI = new javax.swing.JFormattedTextField(teiFormatter);
+        txtTEI = new javax.swing.JFormattedTextField();
         lblPIN = new javax.swing.JLabel();
-        txtPIN = new javax.swing.JFormattedTextField(pinFormatter);
+        txtPIN = new javax.swing.JFormattedTextField();
+        lblPEI = new javax.swing.JLabel();
+        txtPEI = new javax.swing.JFormattedTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("SCIPIO INFORMATION");
@@ -83,7 +91,7 @@ public class ScipioDialog extends POSDialog {
         btnOk.setText("OK");
         btnOk.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                doOk(evt);
+                btnOkdoOk(evt);
             }
         });
 
@@ -91,7 +99,7 @@ public class ScipioDialog extends POSDialog {
         btnCancel.setText("CANCEL");
         btnCancel.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                doCancel(evt);
+                btnCanceldoCancel(evt);
             }
         });
 
@@ -113,6 +121,20 @@ public class ScipioDialog extends POSDialog {
             }
         });
 
+        lblPEI.setText("PEI:");
+        lblPEI.setMaximumSize(new java.awt.Dimension(32, 16));
+        lblPEI.setMinimumSize(new java.awt.Dimension(16, 16));
+        lblPEI.setPreferredSize(new java.awt.Dimension(32, 16));
+
+        txtPEI.setText("123456");
+        txtPEI.setMaximumSize(new java.awt.Dimension(250, 30));
+        txtPEI.setMinimumSize(new java.awt.Dimension(24, 20));
+        txtPEI.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtPEIActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -121,6 +143,10 @@ public class ScipioDialog extends POSDialog {
             .addGroup(layout.createSequentialGroup()
                 .addGap(39, 39, 39)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblPEI, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtPEI, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(lblPIN, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -141,18 +167,22 @@ public class ScipioDialog extends POSDialog {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(titlePanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtPEI, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblPEI, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(lblTEI, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(5, 5, 5)
-                        .addComponent(txtTEI, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addComponent(txtTEI, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(lblPIN, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(5, 5, 5)
                         .addComponent(txtPIN, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnOk, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnCancel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -164,20 +194,33 @@ public class ScipioDialog extends POSDialog {
 
 	public void initData() {
 		System.out.println("...in ScipioDialog.initData()");
+		System.out.println("ticket: " + ticket.getId());
 	}
-	
+
+	// temporarily added to workaround Netbeans dialog code generation
+	// TODO: edit to just use doOk
+	private void btnOkdoOk(java.awt.event.ActionEvent evt) {
+		doOk(evt);
+	}
 	private void doOk(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOkdoOk
 		try {
 			System.out.println("...in ScipioDialog.doOk()");
 		
+			
+			//TODO: attach TIE & PIN to ticket
+			
 			setCanceled(false);
 			dispose();
-			//TODO: attach TIE & PIN to ticket
 		} catch (PosException e) {
 			POSMessageDialog.showError(this, e.getMessage());
 		}
 	}//GEN-LAST:event_btnOkdoOk
 
+	// temporarily added to workaround Netbeans dialog code generation
+	// TODO: edit to just use doCancel
+	private void btnCanceldoCancel(java.awt.event.ActionEvent evt) {
+		doCancel(evt);
+	}
 	private void doCancel(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCanceldoCancel
 		System.out.println("... inScipioDialog.doCancel()");
 		dispose();
@@ -186,6 +229,10 @@ public class ScipioDialog extends POSDialog {
 	private void txtPINActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPINActionPerformed
 		// TODO add your handling code here:
 	}//GEN-LAST:event_txtPINActionPerformed
+
+	private void txtPEIActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPEIActionPerformed
+		// TODO add your handling code here:
+	}//GEN-LAST:event_txtPEIActionPerformed
 
 
 	public Ticket getTicket() {
