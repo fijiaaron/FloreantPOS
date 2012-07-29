7cff0fa6d4f682627f1a5a1e91e70ea1170458c8
Aaron Evans <fijiaaron@fast-mail.org>
2012-07-28 22:46:19 -0600

    readded database directory

--- floreantpos/resources/update_posdb.bat
+++ floreantpos/resources/update_posdb.bat
@@ -1,1 +1,1 @@
-java -Dij.connection.posdb=jdbc:derby://localhost:1527/posdb;username=app;password=sa -jar derbyrun.jar ij  scipio-info.ddl.sql
+java -Dij.connection.posdb=jdbc:derby://localhost:1527/posdb;username=app;password=sa -jar database/lib/derbyrun.jar ij  database/scipio-info.ddl.sql
